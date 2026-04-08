0e9819603b68d1bc56ecad0f254726cb5d702788
Matt G <mjkgarrow@gmail.com>
2026-04-08 10:22:54 +1000

    added netlify adapter and toml

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,9 +9,11 @@ const config = {
 		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
 	},
+	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
-			fallback: '200.html' // may differ from host to host
+			edge: false,
+			split: false
 		})
 	}
 };
